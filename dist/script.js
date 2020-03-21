0ca91004f7abb47b82b707eed3b03bb144b6e0f7
nat-davydova <natti.davydova@gmail.com>
2020-03-21 19:00:00 +0300

    Custom cursor: binding basic handlers to elems with effects

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,5 @@
-//create a cursor element
+const cursorChangeElems = document.querySelectorAll("[data-cursor-type]");
+
 const createCursorElem = () => {
   const customCursor = document.createElement("div");
   customCursor.classList.add("c-cursor");
@@ -13,9 +14,21 @@ const moveCursorElem = (x, y) => {
   requestAnimationFrame(moveCursorElem);
 };
 
-//cursor function
+const setHovers = changeElemsArr => {
+  changeElemsArr.forEach(elem => {
+    const behavioursArr = elem.dataset.cursorType.split(" ");
+
+    elem.addEventListener("mouseenter", hoverHandler.bind(behavioursArr));
+
+    elem.addEventListener("mouseout", outHandler);
+  });
+};
+
+//cursor init function
 const initCursor = () => {
   createCursorElem();
+
+  setHovers(cursorChangeElems);
 
   document.body.addEventListener("mousemove", e => {
     moveCursorElem(e.clientX, e.clientY);
